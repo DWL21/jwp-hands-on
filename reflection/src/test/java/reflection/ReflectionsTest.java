e24a32bcbc7942b2599e5091451944a3bdaab7ea
LEO <nggus5@naver.com>
2022-09-17 17:51:38 +0900

    📖 자바 리플렉션 사용하기 (#4)
    * 요구사항 작성
    
    * 1단계 요구사항 완료
    
    * 2단계 요구사항 완료
    
    * 인스턴스 생성 리플렉션 사용
    
    * 3단계 요구사항 완료
    
    * 4단계 요구사항 완료

--- reflection/src/test/java/reflection/ReflectionsTest.java
+++ reflection/src/test/java/reflection/ReflectionsTest.java
@@ -1,5 +1,14 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
@@ -10,9 +19,21 @@ class ReflectionsTest {
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> targets = new HashSet<>();
+        targets.addAll(reflections.getTypesAnnotatedWith(Controller.class));
+        targets.addAll(reflections.getTypesAnnotatedWith(Service.class));
+        targets.addAll(reflections.getTypesAnnotatedWith(Repository.class));
+
+        List<String> targetNames = targets.stream()
+            .map(Class::getSimpleName)
+            .collect(Collectors.toList());
+
+        for (String name : targetNames) {
+            log.info(name);
+        }
     }
 }
